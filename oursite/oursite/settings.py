ba324133a30d2ffd7ff6c3aa499c36ecfb081f0b
Daniil Lebedev <daniilwork247@gmail.com>
2021-06-01 14:20:20 +0100

    make it better

--- oursite/oursite/settings.py
+++ oursite/oursite/settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
