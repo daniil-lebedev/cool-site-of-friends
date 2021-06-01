1d6f736989334771b3c43858a8dd998a368089c6
Daniil Lebedev <daniilwork247@gmail.com>
2021-06-01 13:11:57 +0100

    First commit

--- oursite/oursite/settings.py
+++ oursite/oursite/settings.py
@@ -134,13 +134,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 #  Add configuration for static files storage using whitenoise
